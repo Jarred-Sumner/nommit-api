e627e5ea3eb8aff3610c161fdcd393382f5d52a7
Jarred Sumner <jarred@jarredsumner.com>
2014-09-06 18:24:51 -0700

    Enable orders routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :foods, only: [:index, :show]
   resources :sessions, only: [:create, :destroy]
   resources :users, only: [:create]
+  resources :orders
 end
