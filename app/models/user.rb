d099673c9333669e86d2dbd1943f4fd0de9f5932
Jarred Sumner <jarred@jarredsumner.com>
2014-09-13 16:29:25 -0700

    Fix Authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,27 +2,29 @@ class User < ActiveRecord::Base
   has_many :orders
   belongs_to :address
 
-  def update_from_facebook!
-    facebook = Koala::Facebook::API.new(access_token)
-    user = facebook.get_object("me")
+  attr_accessor :facebook
 
-    self.facebook_uid = user['id']
-    self.email = user['email']
-    self.name = user['name']
-  rescue Koala::Facebook::AuthenticationError
-    errors.add(:access_token, "is unauthorized")
-  end
+  def self.from(access_token: nil)
+    unless user = User.find_by(access_token: access_token)
+      facebook = Koala::Facebook::API.new(access_token).get_object("me")
+      user = User.where(facebook_uid: facebook['id']).first_or_initialize
+      return user if user.persisted?
 
-  def self.authenticate_or_create!(access_token)
-    user = User.where(access_token: access_token).first_or_create!
-    user.touch(:last_signin)
+      user.facebook_uid = facebook['id']
+      user.email = facebook['email']
+      user.name = facebook['name']
+      user.save!
+    end
     user
   end
 
-  before_validation :update_from_facebook!, on: :create
+  def self.authenticate_or_create!(access_token)
+    user = User.from(access_token: access_token)
+    user.update_attributes!(last_signin: DateTime.now, access_token: access_token)
+    user
+  end
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :access_token, presence: true
   validates :facebook_uid, presence: true, uniqueness: true
 end
