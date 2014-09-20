58d4c1a34a1af0e7099f775c21e94f8deba673ae
Jarred Sumner <jarred@jarredsumner.com>
2014-09-20 14:49:23 -0700

    Start work on seeds for Place

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,213 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+PLACES = {
+  "Boss House" : {
+
+  },
+  "Doherty Apartments": {
+
+  },
+  "Donner House": {
+
+  },
+  "Fairfax Apartments": {
+
+  },
+  "Fraternity/Sorority Quadrangle": {
+
+  },
+  "Hamerschlag House": {
+
+  },
+  "Henderson House": {
+
+  },
+  "Margaret Morrison Apartments/Plaza": {
+
+  },
+  "Margaret Morrison Fraternity" : {
+
+  },
+  "McGill House": {
+
+  },
+  "Morewood Gardens (Housing Offices)": {
+
+  },
+  "Mudge House" : {
+
+  },
+  "Neville Apartments" : {
+
+  },
+  "Resnik House" : {
+
+  },
+  "Roselawn Houses" : {
+
+  },
+  "Scobell House" : {
+
+  },
+  "Shady Oak Apartments" : {
+
+  },
+  "Shirley Apartments" : {
+
+  },
+  "Spirit House" : {
+
+  },
+  "Stever House" : {
+
+  },
+  "The Residence on Fifth" : {
+
+  },
+  "Webster Hall" : {
+
+  },
+  "Welch House" : {
+
+  },
+  "West Wing" : {
+
+  },
+  "Woodlawn Apartments" : {
+
+  },
+  "99 Gladstone" : {
+
+  },
+  "1094 Devon" : {
+
+  },
+  "Alumni House" : {
+
+  },
+  "Art Park" : {
+
+  },
+  "Baker Hall (Dietrich/H&SS)" : {
+
+  },
+  "Porter Hall" : {
+
+  },
+  "Bramer House" : {
+
+  },
+  "College of Fine Arts (CFA)" : {
+
+  },
+  "Cyert Hall" : {
+
+  },
+  "Doherty Hall" : {
+
+  },
+  "Facilities Management Services Building" : {
+
+  },
+  "Gates Center for Computer Science (SCS)" : {
+
+  },
+  "Hillman Center for Future-Generation Technologies (SCS)" : {
+
+  },
+  "Hamburg Hall (Heinz)" : {
+
+  },
+  "Hamerschlag Hall" : {
+
+  },
+  "Hunt Library" : {
+
+  },
+  "Jared L. Cohon University Center" : {
+
+  },
+  "Margaret Morrison Carnegie Hall" : {
+
+  },
+  "Mellon Institute (MCS)" : {
+
+  },
+  "National Robotics Engineering Center (NREC)" : {
+
+  },
+  "Newell-Simon Hall (SCS)" : {
+
+  },
+  "Pittsburgh Technology Center* (ETC)" : {
+
+  },
+  "Posner Center" : {
+
+  },
+  "Posner Hall (Tepper)" : {
+
+  },
+  "Purnell Center for the Arts" : {
+
+  },
+  "Rand Building" : {
+
+  },
+  "Roberts Engineering Hall" : {
+
+  },
+  "Robert Mehrabian Collaborative" : {
+
+  },
+  "Scaife Hall (CIT)" : {
+
+  },
+  "Future Home of Sherman and Joyce Bowie Scott Hall" : {
+
+  },
+  "Skibo Gymnasium" : {
+
+  },
+  "Smith Hall" : {
+
+  },
+  "Software Engineering Institute (SEI)" : {
+
+  },
+  "Solar Decathlon House" : {
+
+  },
+  "Warner Hall (Office of Admission)" : {
+
+  },
+  "Wean Hall" : {
+
+  },
+  "Whitfield Hall (HR)" : {
+
+  },
+  "300 South Craig (Police)" : {
+
+  },
+  "311 South Craig" : {
+
+  },
+  "407 South Craig" : {
+
+  },
+  "4516 Henry (UTDC)" : {
+
+  },
+  "4609 Henry (Dietrich/H&SS Grad Labs)" : {
+
+  },
+  "4615 Forbes (GATF)" : {
+
+  },
+  "4616 Henry (INI)" : {
+
+  },
+  "6555 Penn*" : {
+
+  },
+}
