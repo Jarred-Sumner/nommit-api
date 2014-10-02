05caa76c84e14b2c619d28354faf8541b5994f6b
Jarred Sumner <jarred@jarredsumner.com>
2014-10-02 04:23:59 -0700

    Delivery place state, not Shift state

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -21,7 +21,7 @@ class ShiftsController < ApplicationController
       if Integer(shift_params[:state_id]) == Shift.states[:ended]
         if shift.orders.active.count > 0
           shift.halt!
-          shift.delivery_places.update_all(state: Shift.states[:halt])
+          shift.delivery_places.update_all(state: DeliveryPlace.states[:halted])
           return render_error(status: :unprocessable_entity, text: "Please fulfill the remaining #{shift.orders.active.count} order(s) before ending your shift.")
         else
           shift.ended!
