abd2db6f737f9f1d9241fe23ae8713d66c4f4fb6
Jarred Sumner <jarred@jarredsumner.com>
2014-10-22 23:06:41 -0400

    Fix tests in UsersController

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -4,21 +4,60 @@ describe Api::V1::UsersController, type: :controller do
   render_views
 
   describe "#update" do
-    context "registered user" do
-      let(:user) { create(:registered_user) }
-      let(:session) { create(:session, user_id: user.id ) }
-      let(:phone) { user.phone }
+    let(:user) { create(:user) }
+    let(:session) { create(:session, user_id: user.id ) }
 
-      before :each do
-        request.headers["X-SESSION-ID"] = session.token
-        allow_any_instance_of(Texter).to receive(:perform)
+    before :each do
+      request.headers["X-SESSION-ID"] = session.token
+      allow_any_instance_of(Texter).to receive(:perform)
+    end
+
+    context "given a valid token" do
+      before { StripeMock.start }
+      after { StripeMock.stop }
+
+      let(:token) { "void_card_token" }
+
+      it "creates a payment method" do
+        expect do
+          put :update, stripe_token: token, id: user.id
+        end.to change(PaymentMethod, :count)
+        expect(response.status).to eq(200)
+
+        json = JSON.parse(response.body)
+        expect(json['payment_authorized']).to eq(true)
       end
+
+    end
+
+    context "given invalid token" do
+      let(:token) { "void_card_token" }
+
+      it "doesn't create a payment method" do
+        expect do
+          put :update, stripe_token: token, id: user.id
+        end.to_not change(PaymentMethod, :count)
+        expect(response.status).to eq(400)
+      end
+
+    end
+
+    context "registered user" do
+      let(:user) { create(:registered_user, phone: nil) }
+      let(:phone) { Faker::PhoneNumber.cell_phone }
 
       it "changes phone" do
         put :update, id: user.id, phone: phone
         expect(response.status).to eq(200)
-        expect(user.reload.phone).to eq(phone)
+        expect(user.reload.phone).to eq(PhonyRails.normalize_number(phone, default_country_code: "US"))
         expect(Sms::ConfirmCodeSender.jobs.size).to eq(1)
+      end
+
+      it "raises error on invalid phone phone" do
+        put :update, id: user.id, phone: "bagels."
+        expect(response.status).to eq(400)
+        expect(user.reload.phone).to eq(nil)
+        expect(Sms::ConfirmCodeSender.jobs.size).to eq(0)
       end
 
       it "sets User to activated on confirm" do
@@ -37,35 +76,6 @@ describe Api::V1::UsersController, type: :controller do
         expect(Sms::ConfirmCodeSender.jobs.size).to eq(1)
       end
 
-      context "given a valid token" do
-        before { StripeMock.start }
-        after { StripeMock.stop }
-
-        let(:token) { "void_card_token" }
-
-        it "creates a payment method" do
-          expect do
-            put :update, stripe_token: token, id: user.id
-          end.to change(PaymentMethod, :count)
-          expect(response.status).to eq(200)
-
-          json = JSON.parse(response.body)
-          expect(json['payment_authorized']).to eq(true)
-        end
-
-      end
-
-      context "given invalid token" do
-        let(:token) { "void_card_token" }
-
-        it "doesn't create a payment method" do
-          expect do
-            put :update, stripe_token: token, id: user.id
-          end.to_not change(PaymentMethod, :count)
-          expect(response.status).to eq(400)
-        end
-
-      end
     end
 
   end
