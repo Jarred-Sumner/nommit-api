70938491885f6a3077ee69d2469f093818cd620f
Jarred Sumner <jarred@jarredsumner.com>
2014-11-16 19:25:46 -0500

    Minor copy changes

--- app/mailers/foods_mailer.rb
+++ app/mailers/foods_mailer.rb
@@ -1,12 +1,12 @@
 class FoodsMailer < ActionMailer::Base
-  default from: "support@getnommit.com"
+  default from: '"Nommit" <support@getnommit.com>'
 
   def new(food_id, user_id)
     @user = User.emailable.find(user_id)
     @food = Food.orderable.find(food_id)
 
     if @food.seller.name.include?("Nommit")
-      @subject = "Get #{@food.title} delivered to CMU right now!"
+      @subject = "Get #{@food.title} delivered to CMU"
     else
       @subject = "#{@food.seller.name} is delivering #{@food.title} right now!"
     end
