86bf082d3b97dcfe7252d3a83048d7bc82b8e175
Jarred Sumner <jarred@jarredsumner.com>
2014-11-20 01:37:40 -0500

    Rename statement description to make it static

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,20 +1,20 @@
 class Api::V1::SubscriptionsController < Api::V1::ApplicationController
 
   def create
-    if update_params[:sms].present?
+    if create_params[:sms].present?
       current_user.subscription ||= Subscription.create!(user_id: current_user.id)
       values = [0, 1]
-      state = Integer(update_params[:sms])
+      state = Integer(create_params[:sms])
       if values.include?(state)
         state = state == 1 ? true : false
         current_user.subscription.update_attributes!(sms: state)
       end
     end
 
-    if update_params[:email].present?
+    if create_params[:email].present?
       current_user.subscription ||= Subscription.create!(user_id: current_user.id)
       values = [0, 1]
-      state = Integer(update_params[:email])
+      state = Integer(create_params[:email])
       if values.include?(state)
         state = state == 1 ? true : false
         current_user.subscription.update_attributes!(email: state)
@@ -33,7 +33,7 @@ class Api::V1::SubscriptionsController < Api::V1::ApplicationController
       current_user.subscription
     end
 
-    def update_params
+    def create_params
       params.permit(:sms, :email, :user_id)
     end
 
